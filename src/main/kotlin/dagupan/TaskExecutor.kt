3b5b299f73165ad9d21ee2393aa06fdf2129c34d
diarmuidkeane <diarmuidkeane@gmail.com>
2019-12-27 00:55:47 +0000

    exception and cancellation handling

--- src/main/kotlin/dagupan/TaskExecutor.kt
+++ src/main/kotlin/dagupan/TaskExecutor.kt
@@ -6,31 +6,52 @@ import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
 
 class TaskExecutor {
 
+    private val resultMap = mutableMapOf<Task, Deferred<Unit>>()
+
     fun execute(taskSet: Set<Task>, dispatcher: CoroutineDispatcher = Dispatchers.Default) {
         runBlocking(dispatcher) {
-            val resultMap = mutableMapOf<Task, Deferred<Unit>>()
-
-            fun fromTaskAsync(task: Task): Deferred<Unit> {
-                val existingResult = resultMap[task]
-                return if (existingResult != null) existingResult
-                else {
+            supervisorScope {
+                 fun prepareAsync(task: Task): Deferred<Unit> {
                     resultMap[task] = async(start = CoroutineStart.LAZY) {
                         task.dependsOn.forEach {
-                            resultMap[it]?.run { if (!isActive) { join() } }
+                            resultMap[it]?.run {
+                                if (!isActive) {
+                                    join()
+                                }
+                            }
                         }
                         task.doWork()
                     }
-                    resultMap[task]!!
+                    return  resultMap[task]!!
                 }
-            }
 
-            taskSet.map { fromTaskAsync(it) }
-                    .forEach { it.await() }
+                fun fromTaskAsync(task: Task): Deferred<Unit> {
+                    return resultMap[task]?.let { it } ?: prepareAsync(task)
+                }
+
+                taskSet.map { fromTaskAsync(it) }
+                        .forEach { kickOff(it) }
+            }
         }
     }
 
+    private suspend fun kickOff(deferred: Deferred<Unit>) {
+        runCatching {
+            reverseLookup(deferred).dependsOn.forEach {
+                resultMap[it]?.run {
+                    if (isCancelled) {
+                        deferred.cancel()
+                    }
+                }
+            }
+            if(!deferred.isCancelled) deferred.await()
+        }.onFailure { /**/ }.onSuccess { /**/ }
+    }
 
+    private fun reverseLookup(deferred: Deferred<Unit>) =
+            resultMap.filterValues { it == deferred }.keys.first()
 }
--- src/test/kotlin/dagupan/TaskExecutorTest.kt
+++ src/test/kotlin/dagupan/TaskExecutorTest.kt
@@ -21,17 +21,16 @@ class TaskExecutorTest {
 
     @BeforeEach
     fun setup(){
-        val task0 = Task(emptySet()) { testList.add("task0") } .also { taskMap["task0"] = it  }
-        val task1 = Task(emptySet()) { testList.add("task1") } .also { taskMap["task1"] = it  }
-        val task2 = Task(emptySet()) { testList.add("task2") } .also { taskMap["task2"] = it  }
-        val task3 = Task(setOf(task1)) { testList.add("task3") } .also { taskMap["task3"] = it   }
-        val task4 = Task(emptySet()) { testList.add("task4") } .also { taskMap["task4"] = it   }
-        val task5 = Task(emptySet()) { testList.add("task5") } .also { taskMap["task5"] = it   }
-        val task6 = Task(setOf(task3, task1)) { testList.add("task6") } .also { taskMap["task6"] = it  }
-        val task7 = Task(setOf(task4, task1)) { testList.add("task7") } .also { taskMap["task7"] = it  }
-        val task8 = Task(setOf(task2, task7)) { testList.add("task8") } .also { taskMap["task8"] = it  }
-        val task9 = Task(emptySet()) { testList.add("task9") } .also { taskMap["task9"] = it   }
-
+        val task0 = Task(emptySet()) { testList.add("task0") ; println( Thread.currentThread().name)} .also { taskMap["task0"] = it  }
+        val task1 = Task(emptySet()) { testList.add("task1") ; println( Thread.currentThread().name)} .also { taskMap["task1"] = it  }
+        val task2 = Task(emptySet()) { testList.add("task2") ; println( Thread.currentThread().name)} .also { taskMap["task2"] = it  }
+        val task3 = Task(setOf(task1)) { testList.add("task3") ; println( Thread.currentThread().name)} .also { taskMap["task3"] = it   }
+        val task4 = Task(emptySet()) { testList.add("task4") ; println( Thread.currentThread().name)} .also { taskMap["task4"] = it   }
+        val task5 = Task(emptySet()) { testList.add("task5") ; println( Thread.currentThread().name)} .also { taskMap["task5"] = it   }
+        val task6 = Task(setOf(task3, task1)) { testList.add("task6") ; println( Thread.currentThread().name)} .also { taskMap["task6"] = it  }
+        val task7 = Task(setOf(task4, task1)) { testList.add("task7") ; println( Thread.currentThread().name)} .also { taskMap["task7"] = it  }
+        val task8 = Task(setOf(task2, task7)) { testList.add("task8") ; println( Thread.currentThread().name)} .also { taskMap["task8"] = it  }
+        val task9 = Task(emptySet()) { testList.add("task9") ; println( Thread.currentThread().name)} .also { taskMap["task9"] = it   }
     }
 
     @AfterEach
@@ -48,6 +47,47 @@ class TaskExecutorTest {
         assertThat(testList.indexOf("task6"), greaterThan(testList.indexOf("task3")));
         assertThat(testList.indexOf("task6"), greaterThan(testList.indexOf("task1")));
         assertThat(testList.indexOf("task3"), greaterThan(testList.indexOf("task1")));
+        assertThat(testList.indexOf("task7"), greaterThan(testList.indexOf("task4")));
+        assertThat(testList.indexOf("task7"), greaterThan(testList.indexOf("task1")));
+        assertThat(testList.indexOf("task8"), greaterThan(testList.indexOf("task7")));
+        assertThat(testList.indexOf("task8"), greaterThan(testList.indexOf("task2")));
+    }
+
+    @Test
+    fun `execute when independent workload fails the other workloads are not disturbed`() {
+        taskMap["task0"] =  Task(emptySet()) {throw RuntimeException()}
+        TaskExecutor().execute(taskMap.values.toSet())
+        assertThat(testList, hasSize(9))
+        assertThat(testList, containsInAnyOrder("task1", "task2", "task3", "task4", "task5", "task6", "task7", "task8", "task9"))
+        assertThat(testList.indexOf("task6"), greaterThan(testList.indexOf("task3")));
+        assertThat(testList.indexOf("task6"), greaterThan(testList.indexOf("task1")));
+        assertThat(testList.indexOf("task3"), greaterThan(testList.indexOf("task1")));
+        assertThat(testList.indexOf("task7"), greaterThan(testList.indexOf("task4")));
+        assertThat(testList.indexOf("task7"), greaterThan(testList.indexOf("task1")));
+        assertThat(testList.indexOf("task8"), greaterThan(testList.indexOf("task7")));
+        assertThat(testList.indexOf("task8"), greaterThan(testList.indexOf("task2")));
+    }
+
+    @Test
+    fun `execute when a workload dependency fails the dependent workload also fails but the other workloads are not disturbed`() {
+        val newTask1  = Task(emptySet()) { println( Thread.currentThread().name) ;throw RuntimeException()}.also {  taskMap["task1"] = it}
+        val newTask3 =  Task(setOf(newTask1)) { testList.add("task3"); println( Thread.currentThread().name) }.also {  taskMap["task3"] = it}
+        Task(setOf(newTask3, newTask1)) { testList.add("task6"); println( Thread.currentThread().name) } .also { taskMap["task6"] = it  }
+        TaskExecutor().execute(taskMap.values.toSet())
+        assertThat(testList, hasSize(7))
+        assertThat(testList, containsInAnyOrder("task0","task2", "task4", "task5", "task7", "task8", "task9"))
+        assertThat(testList.indexOf("task7"), greaterThan(testList.indexOf("task4")));
+        assertThat(testList.indexOf("task8"), greaterThan(testList.indexOf("task7")));
+        assertThat(testList.indexOf("task8"), greaterThan(testList.indexOf("task2")));
+    }
+
+    @Test
+    fun `execute when a workload dependency fails the dependent workload also fails but it's own dependencies and the other workloads are not disturbed`() {
+        val newTask3 =  Task(setOf(taskMap["task1"]!! )) { println( Thread.currentThread().name) ;throw RuntimeException()}.also {  taskMap["task3"] = it}
+        Task(setOf(newTask3, taskMap["task1"]!! )) { testList.add("task6"); println( Thread.currentThread().name) } .also { taskMap["task6"] = it  }
+        TaskExecutor().execute(taskMap.values.toSet())
+        assertThat(testList, hasSize(8))
+        assertThat(testList, containsInAnyOrder("task0","task1","task2", "task4", "task5", "task7", "task8", "task9"))
         assertThat(testList.indexOf("task7"), greaterThan(testList.indexOf("task4")));
         assertThat(testList.indexOf("task7"), greaterThan(testList.indexOf("task1")));
         assertThat(testList.indexOf("task8"), greaterThan(testList.indexOf("task7")));
